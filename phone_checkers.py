1d783e9b98300eceeb82ff81fbc72bb80e4fe0e9
Mitry <master2898thetimelord@gmail.com>
2021-06-06 18:48:56 +0300

    refactored; added 'ktozvonil.net' resource

--- phone_checkers.py
+++ phone_checkers.py
@@ -1,6 +1,7 @@
 import logging
 
 import aiohttp
+from aiohttp.client_exceptions import ClientError
 from bs4 import BeautifulSoup
 
 log = logging.getLogger(__name__)
@@ -20,15 +21,25 @@ def phone_checker(resource):
     return decorator
 
 
-@phone_checker("https://ktozvonit.com.ua/")
-async def ktozvonit_check(phone_number, resource=None):
-    reports = list()
-    url = f'{resource}number/{phone_number}/comments/1'
-    response = await _request('GET', url)
+async def _request(method, url):
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.request(method, url) as response:
+                if response.ok:
+                    return await response.text()
+                else:
+                    log.warning(f"Can't get info from {url}: {response.status}")
+    except ClientError as ex:
+        log.error(f"Can't get info from: {url}: {ex}")
 
+
+async def _check(method, url, tag_name, tag_attrs):
+    reports = list()
+
+    response = await _request(method, url)
     if response:
         soup = BeautifulSoup(response, 'html.parser')
-        comments = soup.find_all('p', attrs={'class': 'question-desc'})
+        comments = soup.find_all(tag_name, attrs=tag_attrs)
         for comment in comments:
             if isinstance(comment.contents, list):
                 reports.append("\n".join(comment.contents))
@@ -38,10 +49,14 @@ async def ktozvonit_check(phone_number, resource=None):
     return reports
 
 
-async def _request(method, url):
-    async with aiohttp.ClientSession() as session:
-        async with session.request(method, url) as response:
-            if response.ok:
-                return await response.text()
-            else:
-                log.warning(f"Can't get info from {url}: {response.status}")
+@phone_checker("https://ktozvonit.com.ua/")
+async def ktozvonit_check(phone_number, resource=None):
+    url = f'{resource}number/{phone_number}/comments/1'
+    return await _check('GET', url, 'p', {'class': 'question-desc'})
+
+
+@phone_checker("https://ktozvonil.net/")
+async def ktozvonil_check(phone_number, resource=None):
+    url = f'{resource}nomer/{phone_number}'
+    comment_attrs = {'class': 'content', 'itemprop': 'reviewBody'}
+    return await _check('GET', url, 'div', comment_attrs)
